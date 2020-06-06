b8555665b3d06b61256b2730cdc0b7776e5dd430
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-06-06 12:00:15 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -319,7 +319,7 @@ namespace eureka_blocks {
     }
   }
 //% color="#858585" weight=54 blockId=eureka_moter_d block="ﾓｰﾀｰﾄﾞﾗｲﾊﾞｰ 動き|%mode| |%pin|" group="3_ユーレカ装置"
-  export function eureka_moter_L(mode: moter_d, pin: eureka_denki) {
+  export function eureka_moter_d(mode: moter_d, pin: eureka_denki) {
     switch (pin) {
       case eureka_denki.Aﾎﾟｰﾄ:
         if (mode == moter_d.ﾏｴ) {
